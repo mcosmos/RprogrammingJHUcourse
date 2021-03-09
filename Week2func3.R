413dc36a6e876452ca405452334cde87261bc8fd
Marinos Kosmopoulos <46758187+mcosmos@users.noreply.github.com>
2021-03-09 10:23:25 -0600

    Add files via upload

---
+++ Week2func3.R
@@ -0,0 +1,28 @@
+corr <- function(directory, threshold=0){
+	omega <- complete(directory)
+	z <- omega[[nobs]]
+	k <- c()
+	for (i in z){
+		l<-i>threshold
+		k<-c(k,l)
+	}
+	k
+	omega$valid<-k
+	y <- subset(omega, valid == TRUE)
+	m<-y[[id]]
+	n<-c()
+	for (i in m){
+		ix <- formatC(i, width = 3, flag = "0")
+		pathway <- paste(getwd(), "/", directory, "/", ix, ".csv", sep ="")
+		ab <- read.csv(pathway)
+		a<- ab[["sulfate"]]
+		b<- ab[["nitrate'']]
+		d<- cor(a,b)
+		n<-c(n,d)
+		}
+	n
+	monitor_id <-m
+	correlation_coefficients <- n
+	correlation_matrix <- data.frame(m,n)
+	correlation_matrix
+	}
